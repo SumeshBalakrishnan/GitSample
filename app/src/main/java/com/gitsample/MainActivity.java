6d68aca06816ada68e913d49ff2b42f728b752c0
Asha K Varghese <sumesh.b@emvigotech.com>
2017-07-18 17:19:36 +0530

    second commit 2

--- app/src/main/java/com/gitsample/MainActivity.java
+++ app/src/main/java/com/gitsample/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private void testUpload() {
         Log.i("test", "htest");
         Toast.makeText(this, "dfgdf", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "dgdf", Toast.LENGTH_SHORT).show();
     }
 
 }
