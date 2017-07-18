84bf5fc0477e66da1f81e602a419457cdd7a8857
Asha K Varghese <sumesh.b@emvigotech.com>
2017-07-18 18:23:20 +0530

    in master

--- app/src/main/java/com/gitsample/MainActivity.java
+++ app/src/main/java/com/gitsample/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "dfgdf", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "dgdf", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "3", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "I am in master", Toast.LENGTH_SHORT).show();
     }
 
 }
